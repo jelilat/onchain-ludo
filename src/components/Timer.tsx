b70c075c98a37c33ea4d416e45571fd885a6ef03
Jelilat Anofiu <anofiujelailah@gmail.com>
2024-10-21 15:30:26 +0100

    fix build error

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
 const Timer: React.FC = () => {
-  const [time, setTime] = useState("00:00:00");
+  // const [time, setTime] = useState("00:00:00");
 
   useEffect(() => {
     // Timer logic here
   }, []);
 
-  return <span id="watch">{time}</span>;
+  return <span id="watch">{}</span>;
 };
 
 export default Timer;
